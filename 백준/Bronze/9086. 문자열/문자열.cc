5473d08be463262ab9e0a794a21b4b94b4b64429
Gitgloo <130892737+Gitgloo@users.noreply.github.com>
2024-01-06 01:22:50 +0900

    [Bronze V] Title: 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/9086. 문자열/문자열.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+	int t;
+
+	cin >> t;
+
+
+	while (t--) {
+		string str;
+
+		cin >> str;
+
+		cout << str[0] << str[str.length() - 1] << endl;
+	}
+
+	return 0;
+}
