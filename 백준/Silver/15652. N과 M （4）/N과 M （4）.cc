a7b790b9c71a93f6c95c905dca82a3be12e18958
Gitgloo <130892737+Gitgloo@users.noreply.github.com>
2024-01-17 00:13:27 +0900

    [Silver III] Title: N과 M (4), Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+void recur(int n, int m, int index, int* arr) {
+	if (index == m) {
+		for (int i = 0; i < m; i++) {
+			cout << arr[i] << ' ';
+		}
+		cout << '\n';
+
+		return;
+	}
+
+	for (int i = index ? arr[index - 1] : 1; i <= n; i++) {
+		arr[index] = i;
+		recur(n, m, index + 1, arr);
+	}
+}
+
+int main(void) {
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	int n, m;
+
+	cin >> n >> m;
+	int* arr = new int[n];
+
+	recur(n, m, 0, arr);
+
+	delete[] arr;
+
+	return 0;
+}
