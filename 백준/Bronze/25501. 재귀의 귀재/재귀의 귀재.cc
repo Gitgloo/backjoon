69ef3cc8e37763372cad19b06efede4773bb197c
Gitgloo <130892737+Gitgloo@users.noreply.github.com>
2024-01-16 16:16:03 +0900

    [Bronze II] Title: 재귀의 귀재, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/25501. 재귀의 귀재/재귀의 귀재.cc
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+int recur(string* word, int left, int right) {
+	if (left >= right)
+		return 1;
+	if ((*word)[left] != (*word)[right])
+		return -1;
+	int ret = recur(word, left + 1, right - 1);
+	if (ret > 0)
+		return ret + 1;
+	return ret - 1;
+}
+
+void checkPalindrome(string* word) {
+	int ret = recur(word, 0, (*word).length() - 1);
+	if (ret > 0)
+		cout << 1 << ' ' << ret << '\n';
+	else
+		cout << 0 << ' ' << -ret << '\n';
+}
+
+int main(void) {
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	int n;
+
+	cin >> n;
+	while (n--) {
+		string word;
+
+		cin >> word;
+		checkPalindrome(&word);
+	}
+
+	return 0;
+}
