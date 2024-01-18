bb7580409827e283f4df048b8c779fb087337ef9
Gitgloo <130892737+Gitgloo@users.noreply.github.com>
2024-01-18 11:49:57 +0900

    [Silver II] Title: 신나는 함수 실행, Time: 424 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.cc
@@ -0,0 +1,47 @@
+#include <iostream>
+
+using namespace std;
+
+int w(int a, int b, int c, int dp[21][21][21]);
+
+int main(void) {
+	int dp[21][21][21];
+
+	for (int i = 0; i < 21; i++) {
+		for (int j = 0; j < 21; j++) {
+			for (int k = 0; k < 21; k++) {
+				dp[i][j][k] = -10000000;
+			}
+		}
+	}
+
+	while (true) {
+		int a, b, c;
+
+		cin >> a >> b >> c;
+		if (a == -1 && b == -1 && c == -1)
+			break;
+
+		cout << "w(" << a << ", " << b << ", " << c << ") = " << w(a, b, c, dp) << '\n';
+	}
+
+	return 0;
+}
+
+int w(int a, int b, int c, int dp[21][21][21]) {
+	if (a <= 0 || b <= 0 || c <= 0)
+		return 1;
+
+	if (a > 20 || b > 20 || c > 20)
+		return w(20, 20, 20, dp);
+
+	if (dp[a][b][c] != -10000000)
+		return dp[a][b][c];
+
+	if (a < b && b < c)
+		dp[a][b][c] = w(a, b, c - 1, dp) + w(a, b - 1, c - 1, dp) - w(a, b - 1, c, dp);
+	else
+		dp[a][b][c] = w(a - 1, b, c, dp) + w(a - 1, b - 1, c, dp) + w(a - 1, b, c - 1, dp) - w(a - 1, b - 1, c - 1, dp);
+
+	return dp[a][b][c];
+}
