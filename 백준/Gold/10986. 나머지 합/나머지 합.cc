babe34ed3a39d38959f41e5a153444953004ed94
Gitgloo <130892737+Gitgloo@users.noreply.github.com>
2024-01-20 21:17:56 +0900

    [Gold III] Title: 나머지 합, Time: 128 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/10986. 나머지 합/나머지 합.cc
+++ 백준/Gold/10986. 나머지 합/나머지 합.cc
@@ -2,53 +2,36 @@
 
 using namespace std;
 
-long long int recur(int* arr, int left, int right, int m);
-
 int main(void) {
 	cin.tie(NULL);
 	ios_base::sync_with_stdio(false);
 
-	int n, m;
+	int n, m, before{ 0 };
+	long long int count{ 0 };
 
 	cin >> n >> m;
 
-	int* arr = new int[n + 1];
+	int* mod = new int[m]();
 
-	arr[0] = 0;
 	for (int i = 1; i <= n; i++) {
-		cin >> arr[i];
-		arr[i] = (arr[i] + arr[i - 1]) % m;
+		int temp;
+
+		cin >> temp;
+		before = (temp + before) % m;
+		mod[before]++;
 	}
 
-	cout << recur(arr, 1, n, m) << '\n';
+
+	count += mod[0];
+	for (int i = 0; i < m; i++) {
+		if (mod[i] > 1) {
+			count += (long long int)mod[i] * (mod[i] - 1) / 2;
+		}
+	}
+
+	cout << count << endl;
+
+	delete[] mod;
 
 	return 0;
-}
-
-long long int recur(int* arr, int left, int right, int m) {
-	if (left > right)
-		return 0;
-
-	int mid = (left + right) / 2;
-	long long int count = 0;
-	int* lCount = new int[m]();
-	int* rCount = new int[m]();
-
-	for (int i = left; i <= mid; i++) {
-		lCount[(arr[mid] - arr[i - 1] + m) % m]++;
-	}
-
-	for (int i = mid + 1; i <= right; i++) {
-		rCount[(arr[i] - arr[mid] + m) % m]++;
-	}
-
-	count += (long long int)lCount[0] * (rCount[0] + 1);
-	for (int i = 1; i < m; i++) {
-		count += (long long int)lCount[i] * (rCount[m - i]);
-	}
-
-	delete[] lCount;
-	delete[] rCount;
-
-	return count + recur(arr, left, mid - 1, m) + recur(arr, mid + 1, right, m);
 }
