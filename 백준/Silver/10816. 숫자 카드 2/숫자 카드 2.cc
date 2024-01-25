5d9fe6ebcec5e87f295973a5643b01d05bc6a01f
Gitgloo <130892737+Gitgloo@users.noreply.github.com>
2024-01-25 10:55:28 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 312 ms, Memory: 3976 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
@@ -1,30 +1,75 @@
 #include <iostream>
-#include <map>
+#include <algorithm>
 
 using namespace std;
+
+int under(int find, int* arr, int n);
+int upper(int find, int* arr, int n);
 
 int main(void) {
 	cin.tie(NULL);
 	ios_base::sync_with_stdio(false);
 
 	int n, m;
-	map<int, int> saveMap;
 
 	cin >> n;
-	while (n--) {
-		int temp;
-		
-		cin >> temp;
-		saveMap[temp]++;
+	int* arr = new int[n];
+
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
 	}
+	sort(arr, arr + n);
 
 	cin >> m;
 	while (m--) {
-		int temp;
+		int find;
 
-		cin >> temp;
-		cout << saveMap[temp] << ' ';
+		cin >> find;
+
+		int leftIndex = under(find, arr, n);
+		if (*(arr + leftIndex) != find) {
+			cout << 0 << ' ';
+			continue;
+		}
+		int rightIndex = upper(find, arr, n);
+		cout << rightIndex - leftIndex + 1 << ' ';
+	}
+	cout << '\n';
+
+	delete[] arr;
+	return 0;
+}
+
+int under(int find, int* arr, int n) {
+	int left = 0, right = n - 1;
+
+	while (left <= right) {
+		int mid = (left + right) / 2;
+
+		if (*(arr +mid) < find) {
+			left = mid + 1;
+		}
+		else {
+			right = mid - 1;
+		}
 	}
 
-	return 0;
+	return ++right;
+}
+
+int upper(int find, int* arr, int n) {
+	int left = 0, right = n - 1;
+
+	while (left <= right) {
+		int mid = (left + right) / 2;
+
+		if (arr[mid] > find) {
+			right = mid - 1;
+		}
+		else {
+			left = mid + 1;
+		}
+	}
+
+	return --left;
 }
