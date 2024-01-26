980686c2b1f3e6539162eaca19748dd1007a72b9
Gitgloo <130892737+Gitgloo@users.noreply.github.com>
2024-01-26 11:50:13 +0900

    [Gold IV] Title: 공유기 설치, Time: 132 ms, Memory: 2804 KB -BaekjoonHub

---
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.cc
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main(void) {
+	int n, c, left{ 0 }, right{ 1000000000 };
+
+	cin >> n >> c;
+
+	int* arr = new int[n];
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+
+	sort(arr, arr + n);
+
+	while (left <= right) {
+		int before = arr[0], count = 1;
+		int mid = (left + right) / 2;
+		
+		for (int i = 0; i < n; i++) {
+			if (arr[i] - before >= mid) {
+				before = arr[i];
+				count++;
+			}
+		}
+
+		if (count < c) {
+			right = mid - 1;
+		} else {
+			left = mid + 1;
+		}
+	}
+
+	cout << right << endl;
+
+	delete[] arr;
+
+	return 0;
+}
