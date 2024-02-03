510fea60e24ba32e320f472f3c499d7422727120
Gitgloo <130892737+Gitgloo@users.noreply.github.com>
2024-02-03 22:53:30 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 2, Time: 92 ms, Memory: 9232 KB -BaekjoonHub

---
+++ 백준/Silver/24445. 알고리즘 수업 － 너비 우선 탐색 2/알고리즘 수업 － 너비 우선 탐색 2.cc
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+
+using namespace std;
+
+void bfs(int n, vector<int>* edgeArray, int r);
+
+bool comp(int a, int b) {
+	return a > b;
+}
+
+int main(void) {
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	int n, m, r;
+
+	cin >> n >> m >> r;
+	vector<int>* edgeArray = new vector<int>[n + 1];
+
+	while (m--) {
+		int a, b;
+
+		cin >> a >> b;
+		edgeArray[a].push_back(b);
+		edgeArray[b].push_back(a);
+	}
+
+	for (int i = 1; i <= n; i++) {
+		sort((edgeArray + i)->begin(), (edgeArray + i)->end(), comp);
+	}
+
+	bfs(n, edgeArray, r);
+
+	delete[] edgeArray;
+
+	return 0;
+}
+
+void bfs(int n, vector<int>* edgeArray, int r) {
+	int order = 1;
+	int* visited = new int[n + 1]();
+	queue<int> q;
+
+	*(visited + r) = order++;
+	q.push(r);
+	while (q.size()) {
+		int now = q.front();
+		q.pop();
+		vector<int> edges = *(edgeArray + now);
+		for (int i = 0; i < edges.size(); i++) {
+			int next = edges[i];
+			if (!*(visited + next)) {
+				*(visited + next) = order++;
+				q.push(next);
+			}
+		}
+	}
+
+	for (int i = 1; i <= n; i++) {
+		cout << *(visited + i) << '\n';
+	}
+
+	delete[] visited;
+}
