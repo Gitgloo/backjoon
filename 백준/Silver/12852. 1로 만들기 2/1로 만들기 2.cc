e709f801f14389303e6e6a1a0be5c4b5184aade4
Gitgloo <130892737+Gitgloo@users.noreply.github.com>
2024-02-09 17:00:48 +0900

    [Silver I] Title: 1로 만들기 2, Time: 8 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.cc
+++ 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.cc
@@ -7,32 +7,35 @@ using namespace std;
 
 int main(void) {
 	int n;
-	vector<int> arr, before;
+	vector<int> arr;
 
 	cin >> n;
 	arr.resize(n + 1, INF);
-	before.resize(n + 1);
 	arr[1] = 0;
 	for (int i = 1; i < n; i++) {
 		int next = arr[i] + 1;
 		if (3 * i <= n && next < arr[3 * i]) {
 			arr[3 * i] = next;
-			before[3 * i] = i;
 		}
 		if (2 * i <= n && next < arr[2 * i]) {
 			arr[2 * i] = next;
-			before[2 * i] = i;
 		}
 		if (i + 1 <= n && next < arr[i + 1]) {
 			arr[i + 1] = next;
-			before[i + 1] = i;
 		}
 	}
 
 	cout << arr[n] << '\n';
 	while (n >= 1) {
 		cout << n << ' ';
-		n = before[n];
+		int before = arr[n] - 1;
+		if (n % 3 == 0 && before == arr[n / 3]) {
+			n /= 3;
+		} else if (n % 2 == 0 && before == arr[n / 2]) {
+			n /= 2;
+		} else {
+			n--;
+		}
 	}
 	cout << '\n';
 
