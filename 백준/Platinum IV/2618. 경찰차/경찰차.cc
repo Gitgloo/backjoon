a67c116b4bf1bf9d93ff477bbf7b9eac20d11f5f
Gitgloo <130892737+Gitgloo@users.noreply.github.com>
2024-02-14 23:18:18 +0900

    [Platinum IV] Title: 경찰차, Time: 4 ms, Memory: 4004 KB -BaekjoonHub

--- 백준/Platinum IV/2618. 경찰차/경찰차.cc
+++ 백준/Platinum IV/2618. 경찰차/경찰차.cc
@@ -19,67 +19,63 @@ int main(void) {
 		cin >> events[i].first >> events[i].second;
 	}
 
-	vector<vector<pair<int, int>>> dp(w);
+	vector<vector<int>> dp(w);
 	for (int i = 0; i < w; i++) {
 		dp[i].resize(i + 2);
 	}
-	dp[0][0].first = n - events[2].first + n - events[2].second;
-	dp[0][0].second = 2;
-	dp[0][1].first = events[2].first - 1 + events[2].second - 1;
-	dp[0][1].second = 1;
+	dp[0][0] = n - events[2].first + n - events[2].second;
+	dp[0][1] = events[2].first - 1 + events[2].second - 1;
 	
+	vector<int> orders(w + 2);
+	orders[0] = 1;
+	orders[1] = 0;
 	for (int i = 1; i < w; i++) {
 		pair<int, int> now = events[i + 2];
 		pair<int, int> before = events[i + 1];
 		int diff = ABS(now.first - before.first) + ABS(now.second - before.second);
 		int min = 2147483647;
-		int minOrder = 1;
 		for (int j = 0; j <= i; j++) {
-			dp[i][j].first = dp[i - 1][j].first + diff;
-			dp[i][j].second = dp[i - 1][j].second;
+			dp[i][j] = dp[i - 1][j] + diff;
 			
-			int temp = dp[i - 1][j].first + ABS(now.first - events[j].first) + ABS(now.second - events[j].second);
-			if (temp < min) {
+			int temp = dp[i - 1][j] + ABS(now.first - events[j].first) + ABS(now.second - events[j].second);
+			if (min > temp) {
 				min = temp;
-				minOrder = (dp[i - 1][j].second) % 2 + 1;
+				orders[i + 1] = (orders[j] + 1) % 2;
 			}
 		}
-
-		dp[i][i + 1].first = min;
-		dp[i][i + 1].second = minOrder;
+		dp[i][i + 1] = min;
 	}
 
 	int nowIndex = 0;
-	int minAns = dp[w - 1][0].first;
+	int minAns = dp[w - 1][0];
 	for (int i = 0; i <= w; i++) {
-		if (minAns > dp[w - 1][i].first) {
-			minAns = dp[w - 1][i].first;
+		if (minAns > dp[w - 1][i]) {
+			minAns = dp[w - 1][i];
 			nowIndex = i;
 		}
 	}
 	cout << minAns << '\n';
 	stack<int> order;
-	order.push(dp[w - 1][nowIndex].second);
+	order.push(orders[nowIndex]);
 	for (int i = w - 1; i > 0; i--) {
 		pair<int, int> now = events[i + 2];
 		
 		if (nowIndex == i + 1) {
-			int nowValue = dp[i][nowIndex].first;
-			int nowOrder = dp[i][nowIndex].second;
+			int nowValue = dp[i][nowIndex];
 			for (int j = 0; j < nowIndex; j++) {
-				if (nowValue == dp[i - 1][j].first + ABS(now.first - events[j].first) + ABS(now.second - events[j].second)) {
+				if (nowValue == dp[i - 1][j] + ABS(now.first - events[j].first) + ABS(now.second - events[j].second)) {
 					nowIndex = j;
 					break;
 				}
 			}
 		}
-		order.push(dp[i][nowIndex].second);
+		order.push(orders[nowIndex]);
 	}
 
 	while (order.size()) {
-		cout << order.top() << '\n';
+		cout << order.top() + 1 << '\n';
 		order.pop();
-	}
+	}	
 
 	return 0;
 }
