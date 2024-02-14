1c62eaf946a2716348828d94e69af9344e67943b
Gitgloo <130892737+Gitgloo@users.noreply.github.com>
2024-02-15 02:44:33 +0900

    [Silver III] Title: 칸토어 집합, Time: 252 ms, Memory: 19064 KB -BaekjoonHub

--- 백준/Silver III/4779. 칸토어 집합/칸토어 집합.java
+++ 백준/Silver III/4779. 칸토어 집합/칸토어 집합.java
@@ -1,21 +1,17 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-/*
-짝대기로 초기화하고 빈칸이어야 하는 곳을 찾아서 빈 칸으로 바꾸기 보다는
-sb를 길이만큼 선언만 하고
-짝대기인 곳은 짝대기로
-빈 칸인 곳은 빈 칸으로하면
-빈 칸이어야 하는 곳을 짝대기로 선언하는 과정이 없어져서 좋을거 같다고 생각했어
+import java.io.*;
 
-그리고 N은 어차피 3^N으로 쓸거니깐 그냥 총 길이(3^N) 변수 하나 추가했어 
+/*
+근데 굳이 sb 안쓰고 char 배열이나 ArrayList<Char> 써도 될 거 같아서 해봤어
+변수 이름은 그냥 그대로 쓸게
 * */
 public class Main {
-    static StringBuilder sb = new StringBuilder();
+//    static StringBuilder sb = new StringBuilder();
+    static char[] sb = null;
 
     public static void main(String[] args) throws IOException {
         try{
             BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
             String str;
             while((str = br.readLine())!=null){
                 int N = Integer.parseInt(str);
@@ -23,12 +19,13 @@ public class Main {
 //              int len = (int)Math.pow(3, Integer.parseInt(str));  // N변수 지우고 이렇게 해도 되고
 //                sb.setLength(0);
 //                sb.setLength((int)Math.pow(3, N));  // 그래서 아예 처음부터 필요한 길이만큼 선언하고
-                sb.setLength(len);  // 그래서 아예 처음부터 필요한 길이만큼 선언하고
+//                sb.setLength(len);  // 그래서 아예 처음부터 필요한 길이만큼 선언하고
+                sb = new char[len];
 
                 /*
                 처음에 모두 '-'로 초기화 하지 않고
                 makeCantorSet 함수에서 짝대기인 곳은 짝대기로, 빈 칸은 빈 칸으로
-//                for(int i = 0; i < Math.pow(3, N); i++){ 
+//                for(int i = 0; i < Math.pow(3, N); i++){
                 for(int i = 0; i < len; i++){
 //                    sb.append("-");
                     sb.setCharAt(i, '-');   // 모두 '-'로 초기화
@@ -40,7 +37,12 @@ public class Main {
 //                makeCantorSet(0, sb.length());
                 makeCantorSet(0, len);  // len으로 대체!
 //                System.out.println(sb.toString());
-                System.out.println(sb); // sb 그냥 이렇게 해도 될거야 그 오버라이딩인가 오버로딩인가 돼있을거니깐
+//                System.out.println(sb); // sb 그냥 이렇게 해도 될거야 그 오버라이딩인가 오버로딩인가 돼있을거니깐
+                for(char ch : sb){
+                    bw.write(ch);
+                }
+                bw.write('\n');
+                bw.flush();
             }
 
         }catch(Exception e){
@@ -53,7 +55,8 @@ public class Main {
 //        System.out.println(startIdx + ", " + len + ", " + sb.toString());
 
         if(len == 1){
-            sb.setCharAt(startIdx, '-');    // 추가
+//            sb.setCharAt(startIdx, '-');    // 처음에 짝대기로 초기화 안해줫으니깐 추가
+            sb[startIdx] = '-'; // 배열에 직접 접근
             return;
         }
 
@@ -67,7 +70,7 @@ public class Main {
 
         // 중간
         for(int i = subStrLen; i < subStrLen * 2; i++){
-            sb.setCharAt(startIdx + i, ' ');
+            sb[startIdx + i] = ' '; // 배열에 직접 접근
         }
 
 
