da7473877bf926b4cafbb081b110453b4fc85165
Gitgloo <130892737+Gitgloo@users.noreply.github.com>
2024-02-23 17:14:42 +0900

    [Silver IV] Title: 상근이의 여행, Time: 20 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	int T;
+
+	cin >> T;
+	while (T--) {
+		int n, m;
+
+		cin >> n >> m;
+		while (m--) {
+			int temp;
+
+			cin >> temp >> temp;
+		}
+
+		cout << n - 1 << '\n';
+	}
+	return 0;
+}
