7c4e629d0e619e5df6f2f6d1183509c8ac6cc665
Gitgloo <130892737+Gitgloo@users.noreply.github.com>
2024-03-13 13:02:28 +0900

    [Gold III] Title: 색상환, Time: 4 ms, Memory: 5932 KB -BaekjoonHub

--- 백준/Gold/2482. 색상환/색상환.cc
+++ 백준/Gold/2482. 색상환/색상환.cc
@@ -5,7 +5,7 @@
 
 using namespace std;
 
-int n, k, dp[MAX_N - 2][MAX_N];
+int n, k, dp[MAX_N][MAX_N + 1];
 
 void init();
 void solve();
@@ -17,24 +17,28 @@ int main(void) {
 	return 0;
 }
 
-int recur(int range, int count) {
-	if (dp[range][count] == 0) {
-		int nextCount = count - 1;
-		int possible = 2 * nextCount - 1;
-		long long sum = 0;
-
-		for (int i = range - 2; i >= possible; i--) {
-			sum += recur(i, nextCount);
-		}
-		
-		dp[range][count] = sum % MOD;
-	}
-
-	return dp[range][count];
-}
+//int recur(int range, int count) {
+//	if (dp[range][count] == 0) {
+//		int nextCount = count - 1;
+//		int possible = 2 * nextCount - 1;
+//		long long sum = 0;
+//
+//		for (int i = range - 2; i >= possible; i--) {
+//			sum += recur(i, nextCount);
+//		}
+//		
+//		dp[range][count] = sum % MOD;
+//	}
+//
+//	return dp[range][count];
+//}
 
 void solve() {
-	if (n / 2 < k) {
+	for (int i = 0; i < n; i++) {
+		dp[i][0] = 1;
+		dp[i][1] = i;
+	}
+	/*if (n / 2 < k) {
 		cout << "0\n";
 		return;
 	}
@@ -52,14 +56,17 @@ void solve() {
 		ans += recur(i, nextK);
 	}
 	
-	cout << ans % MOD << '\n';
+	cout << ans % MOD << '\n';*/
+
+	for (int i = 2; i < n; i++) {
+		for (int j = 2; j <= k; j++) {
+			dp[i][j] = ((long long)dp[i - 1][j] + dp[i - 2][j - 1]) % MOD;
+		}
+	}
+
+	cout << ((long long)dp[n - 1][k] + dp[n - 3][k - 1]) % MOD << '\n';
 }
 
 void init() {
 	cin >> n >> k;
-
-	for (int i = 0; i < n - 2; i++) {
-		dp[i][0] = 1;
-		dp[i][1] = i;
-	}
 }
