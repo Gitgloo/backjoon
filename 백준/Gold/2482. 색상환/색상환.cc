71491dae088e31091715a537bcba2213ebcc32c1
Gitgloo <130892737+Gitgloo@users.noreply.github.com>
2024-03-13 13:02:50 +0900

    [Gold III] Title: 색상환, Time: 4 ms, Memory: 5932 KB -BaekjoonHub

--- 백준/Gold/2482. 색상환/색상환.cc
+++ 백준/Gold/2482. 색상환/색상환.cc
@@ -17,46 +17,11 @@ int main(void) {
 	return 0;
 }
 
-//int recur(int range, int count) {
-//	if (dp[range][count] == 0) {
-//		int nextCount = count - 1;
-//		int possible = 2 * nextCount - 1;
-//		long long sum = 0;
-//
-//		for (int i = range - 2; i >= possible; i--) {
-//			sum += recur(i, nextCount);
-//		}
-//		
-//		dp[range][count] = sum % MOD;
-//	}
-//
-//	return dp[range][count];
-//}
-
 void solve() {
 	for (int i = 0; i < n; i++) {
 		dp[i][0] = 1;
 		dp[i][1] = i;
 	}
-	/*if (n / 2 < k) {
-		cout << "0\n";
-		return;
-	}
-
-	if (k == 1) {
-		cout << n << '\n';
-		return;
-	}
-
-	int nextK = k - 1;
-	int possible = nextK * 2 - 1;
-	long long ans = recur(n - 3, nextK);
-
-	for (int i = n - 3; i >= possible; i--) {
-		ans += recur(i, nextK);
-	}
-	
-	cout << ans % MOD << '\n';*/
 
 	for (int i = 2; i < n; i++) {
 		for (int j = 2; j <= k; j++) {
