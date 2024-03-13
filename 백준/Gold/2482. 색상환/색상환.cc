6af9f06a366888a5b194a42a4d624e618b524c8f
Gitgloo <130892737+Gitgloo@users.noreply.github.com>
2024-03-13 12:54:02 +0900

    [Gold III] Title: 색상환, Time: 100 ms, Memory: 5920 KB -BaekjoonHub

--- 백준/Gold/2482. 색상환/색상환.cc
+++ 백준/Gold/2482. 색상환/색상환.cc
@@ -18,7 +18,7 @@ int main(void) {
 }
 
 int recur(int range, int count) {
-	if (dp[range][count] == -1) {
+	if (dp[range][count] == 0) {
 		int nextCount = count - 1;
 		int possible = 2 * nextCount - 1;
 		long long sum = 0;
@@ -61,8 +61,5 @@ void init() {
 	for (int i = 0; i < n - 2; i++) {
 		dp[i][0] = 1;
 		dp[i][1] = i;
-		for (int j = 2; j < n; j++) {
-			dp[i][j] = -1;
-		}
 	}
 }
